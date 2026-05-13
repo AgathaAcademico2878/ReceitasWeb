d8d3fdbfb7ace1ff4dd517593da833e400218c2d
AgathaAcademico2878 <agathanogueira.ch027@academico.ifsul.edu.br>
2026-05-12 21:21:00 -0300

    Primeiro commit

--- api/source/Config/Config.php
+++ api/source/Config/Config.php
@@ -1,4 +1,10 @@
 <?php
 
-const CONF_URL_BASE = "http://localhost:8080/acme-3am"; // URL base do site, geralmente localhost em desenvolvimento
-const CONF_URL_TEST = "http://localhost:8080/acme-3am"; // URL base do site, geralmente localhost em desenvolvimento
+const CONF_URL_BASE = "http://localhost/ReceitasWeb"; // URL base do site, geralmente localhost em desenvolvimento
+const CONF_URL_TEST = "http://localhost/ReceitasWeb"; // URL base do site, geralmente localhost em desenvolvimento
+
+const CONF_DB_HOST = "localhost";
+const CONF_DB_NAME = "receitasweb";
+const CONF_DB_USER = "root";
+const CONF_DB_PASS = "";
+const CONF_DB_PORT = "3306";
